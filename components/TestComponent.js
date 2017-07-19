5efe33de318688851f4a1d16cba5c49bf24a0841
ChrisALee <christopher.a.lee5@gmail.com>
2017-07-18 21:36:54 -0500

    Remove TestComponent.js

--- components/TestComponent.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-exports.default = () => React.createElement("div", null,
-    React.createElement("p", null, "This is from TestComponent!"));
