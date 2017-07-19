7bb780b80d5822f3c8a3b2cac30e30402b0e42e9
ChrisALee <christopher.a.lee5@gmail.com>
2017-07-18 23:38:44 -0500

    Remove dist

--- dist/.next/dist/components/TestComponent.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-exports.default = function () {
-    return React.createElement("div", null, React.createElement("p", null, "This is from TestComponent!"));
-};
-//# sourceMappingURL=TestComponent.js.map
--- dist/.next/dist/pages/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var TestComponent_1 = require("../components/TestComponent");
-exports.default = function () {
-    return React.createElement("div", null, React.createElement("p", null, "Welcome to Twitch Stocks! If you forked and cloned this project and you're trying to learn React, go ahead and mess around with it a bit."), React.createElement(TestComponent_1.default, null));
-};
-//# sourceMappingURL=index.js.map
