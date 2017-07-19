5700520eac480a611afe0d73b02c6f6af69015c1
ChrisALee <christopher.a.lee5@gmail.com>
2017-07-18 21:32:07 -0500

    Convert to TypeScript

---
+++ components/TestComponent.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+exports.default = () => React.createElement("div", null,
+    React.createElement("p", null, "This is from TestComponent!"));
