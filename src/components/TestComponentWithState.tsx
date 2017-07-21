9347781abf400ed4d0a7435856ea260c48002b5a
Unknown <jamiroleander24@gmail.com>
2017-07-21 19:07:29 +0200

    reformatted TestComponentWithState

--- src/components/TestComponentWithState.tsx
+++ src/components/TestComponentWithState.tsx
@@ -3,32 +3,31 @@ import * as React from 'react';
 
 export default class TestComponentWithState extends React.Component<any, any> {
   /**
-     * Called upon class instantiation
-     */
+   * Called upon class instantiation
+   */
   constructor() {
     super();
-
     // initialize the state
     this.state = {};
   }
 
   /**
-     * This method is called once all our children Elements and our Component instances are mounted onto the Native UI.
-     */
+   * This method is called once all our children Elements and our Component instances are mounted onto the Native UI.
+   */
   public componentDidMount() {
-    this.setState({ testValue: 'foo' });
+      this.setState({ testValue: 'foo' });
   }
 
   /**
-     * Renders this component onto the browser
-     */
+   * Renders this component onto the browser
+   */
   public render() {
     return (
       <div>
         <p>This component is using a state!</p>
         <p>
-          The following is stored in and displayed from this components' state:{' '}
-          {this.state.testValue}
+        The following is stored in and displayed from this components' state:{' '}
+        {this.state.testValue}
         </p>
       </div>
     );
