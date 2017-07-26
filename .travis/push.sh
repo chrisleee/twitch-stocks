82285554a296dca36687e45e83d0d958b930c0f7
FalseHonesty <thefalsehonesty@gmail.com>
2017-07-26 13:44:55 -0400

    stopped travis build recursion

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 commit_files() {
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
   git remote add origin-cleanup https://${GH_TOKEN}@github.com/ChrisALee/twitch-stocks.git > /dev/null 2>&1
 
   if ["${TRAVIS_PULL_REQUEST_BRANCH}" = ""]
