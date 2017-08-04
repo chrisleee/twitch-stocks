07c84dc471a0f73c368debf7eec38d0df0fab901
ChrisALee <christopher.a.lee5@gmail.com>
2017-08-04 16:31:26 -0500

    Remove extra paranthesis and brace

--- src/server/routes/channels.ts
+++ src/server/routes/channels.ts
@@ -10,17 +10,14 @@ import { Channel } from '../models/channels';
  * @param router express router
  */
 function route(app: Application, router: Router): void {
-  router
-    .route('/channels')
-    .get((req: Request, res: Response): void => {
-      logger.info(`GET /channels from ${req.ip}`);
-      Channel.find((err: Error, channels): Response => {
-        if (err) {
-          logger.error('Cannot find channels');
-          return res.send(err);
-        }
-        return res.send(channels);
-      });
+  router.route('/channels').get((req: Request, res: Response): void => {
+    logger.info(`GET /channels from ${req.ip}`);
+    Channel.find((err: Error, channels): Response => {
+      if (err) {
+        logger.error('Cannot find channels');
+        return res.send(err);
+      }
+      return res.send(channels);
     });
   });
 }
