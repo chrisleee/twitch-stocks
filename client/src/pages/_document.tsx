aa6577f3db07d65300316c9391dca2c0ec46973f
jallier <justin.hallier+git@gmail.com>
2017-08-15 14:36:37 +1200

    Removed margin on body

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Head, Main, NextScript } from 'next/document';
+// import Head from 'next/head';
 import * as React from 'react';
 import { ServerStyleSheet } from 'styled-components';
 
@@ -13,6 +14,11 @@ export default class MyDocument extends Document<{}> {
           <title>Twitch-Stocks</title>
           {styleTags}
         </Head>
+        <style jsx global> {`
+          body {
+           margin: 0;
+          }
+        `}</style>
         <body>
           <div className="root">
             {main}
